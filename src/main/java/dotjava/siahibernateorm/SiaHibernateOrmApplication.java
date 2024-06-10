17c68b86dcb1b0fa044f9663d910a288ef84b333
Prima Hagantha <81164903+primahagantha@users.noreply.github.com>
2024-06-10 21:04:12 +0700

    #Add: require plugin and program

---
+++ src/main/java/dotjava/siahibernateorm/SiaHibernateOrmApplication.java
@@ -0,0 +1,13 @@
+package dotjava.siahibernateorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiaHibernateOrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiaHibernateOrmApplication.class, args);
+    }
+System.out.prinln("Hello World")
+}
---
+++ src/test/java/dotjava/siahibernateorm/SiaHibernateOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package dotjava.siahibernateorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiaHibernateOrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
