bbe3a5aa03f188c32a1ffecc96fd0c8d856ed118
Rafael Simarmata <80381531+Rafaellsimarmata@users.noreply.github.com>
2024-06-16 16:57:41 +0700

    fixed hibernate.cfg

--- src/main/java/dotjava/siahibernateorm/Score.java
+++ src/main/java/dotjava/siahibernateorm/Score.java
@@ -1,2 +1,53 @@
-package dotjava.siahibernateorm;public class Score {
+package dotjava.siahibernateorm;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "score")
+public class Score {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @PrimaryKeyJoinColumn
+    private Integer score_id;
+
+    @Column (name = "scoreAvg", nullable = false)
+    private Integer score;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    @ManyToOne
+    @JoinColumn(name = "class_id")
+    private Class classEntity;
+
+
+    public Integer getScore_id() {
+        return score_id;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public Class getClassEntity() {
+        return classEntity;
+    }
+
+    public void setClassEntity(Class classEntity) {
+        this.classEntity = classEntity;
+    }
 }
